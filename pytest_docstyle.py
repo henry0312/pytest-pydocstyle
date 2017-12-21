db10045ae31dd7dc0cc43f16e87a54c7a9bb7a7c
Tsukasa OMOTO <henry0312@gmail.com>
2017-12-21 13:18:14 +0900

    Add marker
    https://docs.pytest.org/en/latest/writing_plugins.html#_pytest.main.Node.add_marker

--- pytest_docstyle.py
+++ pytest_docstyle.py
@@ -27,6 +27,7 @@ class Item(pytest.Item, pytest.File):
 
     def __init__(self, path, parent):
         super().__init__(path, parent)
+        self.add_marker('docstyle')
 
     def setup(self):
         old_mtime = self.config.cache.get(self.CACHE_KEY, {}).get(str(self.fspath), -1)
