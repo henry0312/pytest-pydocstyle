f52e00d4999b5c73a53f21eadfc37f7da82b17ef
Tsukasa OMOTO <henry0312@gmail.com>
2018-01-15 11:59:41 +0900

    Release v1.2.1 (#6)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pytest-docstyle',
-    version='1.2.0',
+    version='1.2.1',
     description='pytest plugin to run pydocstyle',
     url='https://github.com/henry0312/pytest-docstyle',
     author='Tsukasa OMOTO',
