ffd115fc81f538649bd5b6ed557ded1b4fadd782
OMOTO Tsukasa <tsukasa@oomo.to>
2019-11-16 14:44:35 +0900

    Add `::DOCSTYLE` after files in pytest verbose mode (#45)
    https://github.com/henry0312/pytest-codestyle/pull/52
    https://github.com/henry0312/pytest-codestyle/pull/54
    https://github.com/henry0312/pytest-codestyle/pull/55

--- pytest_docstyle.py
+++ pytest_docstyle.py
@@ -48,6 +48,11 @@ class Item(pytest.Item, pytest.File):
         super().__init__(path, parent)
         self.add_marker('docstyle')
 
+        # https://github.com/pytest-dev/pytest/blob/92d6a0500b9f528a9adcd6bbcda46ebf9b6baf03/src/_pytest/nodes.py#L380
+        # https://github.com/pytest-dev/pytest/blob/92d6a0500b9f528a9adcd6bbcda46ebf9b6baf03/src/_pytest/nodes.py#L101
+        # https://github.com/moccu/pytest-isort/blob/44f345560a6125277f7432eaf26a3488c0d39177/pytest_isort.py#L142
+        self._nodeid += '::DOCSTYLE'
+
     def setup(self):
         old_mtime = self.config.cache.get(self.CACHE_KEY, {}).get(str(self.fspath), -1)
         mtime = self.fspath.mtime()
